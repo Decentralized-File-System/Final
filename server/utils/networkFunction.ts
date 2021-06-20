a605b607db84e6c72bcb9db412c30205321ed5e7
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-06-20 19:28:48 +0300

    using axios to upload files

--- server/utils/networkFunction.ts
+++ server/utils/networkFunction.ts
@@ -2,17 +2,50 @@ import axios from "axios";
 import { ChunkClass } from "./classes";
 //@ts-ignore
 import nodePorts from "../utils/ports.json";
+const FormData = require("form-data");
 
 export const uploadChunks = async (fileChunkArray: ChunkClass[]) => {
   try {
     const promiseArray = fileChunkArray.map((chunk, i) => {
-      return axios.post(
-        `http://${nodePorts[i].host}:${nodePorts[i].port}/api/v1/file/upload-file`,
-        { data: chunk }
-      );
+      const buffer = Buffer.from(JSON.stringify(chunk));
+      const form = new FormData();
+      form.append("file", buffer);
+      const formHeaders = form.getHeaders();
+      console.log(formHeaders);
+      // form.submit(
+      //   `http://${nodePorts[i].host}:${nodePorts[i].port}/api/v1/file/upload-file`,
+      //   //@ts-ignore
+      //   (err, res) => {
+      //     if (err) {
+      //       console.log(err);
+      //     } else {
+      //       res.resume();
+      //     }
+      //   }
+      // );
+      // return axios.post(
+      //   `http://${nodePorts[i].host}:${nodePorts[i].port}/api/v1/file/upload-file`,
+      //   form,
+      //   {
+      //     headers: formHeaders,
+      //   }
+      // );
+      return axios({
+        method: "post",
+        url: `http://${nodePorts[i].host}:${nodePorts[i].port}/api/v1/file/upload-file`,
+        data: form,
+        maxContentLength: Infinity,
+        maxBodyLength: Infinity,
+        headers: {
+          "Content-Type": "multipart/form-data;boundary=" + form.getBoundary(),
+        },
+      });
     });
-
-    await Promise.all(promiseArray);
+    try {
+      await Promise.all(promiseArray);
+    } catch (error) {
+      console.log(error);
+    }
 
     return { message: "success" };
   } catch (error) {
