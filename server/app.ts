b64bb08c775aacfb8efa41710db54680b8f0f858
omer rokach <77382841+omerokach@users.noreply.github.com>
2021-06-24 17:57:56 +0300

    version 1 auth routes check

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
+import { checkUser } from "./middlewares/authMiddleware";
+import cookieParser from "cookie-parser"
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api", routes);
+app.use("/api", checkUser, routes);
 
 export default app;
--- server/controllers/usersController.ts
+++ server/controllers/usersController.ts
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
 import { user } from "../types";
 import { getUserByUserName, getUserByEmail } from "../utils/DBqueries";
-const secretKey: string = process.env.SECRET_KEY;
+const secretKey: any = process.env.SECRET_KEY;
 
 // create json web token
 const maxAge = 3 * 24 * 60 * 60;
@@ -46,26 +46,31 @@ export const signUp_post = async (req: Request, res: Response) => {
   }
 };
 
-export const login_post = async (req:Request, res:Response) => {
-    const { email, password } = req.body;
+export const login_post = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
 
-    try {
-      const existUser = await getUserByEmail(email);
-      if (!existUser) {
-        return res.status(404).send("Incorrect email or password");
-      }
-      const auth = compareSync(password, existUser.dataValues.password);
-      if (!auth) {
-        return res.status(404).send("Incorrect email or password");
-      }
-      const userToken = {
-        username: existUser.dataValues.userName,
-        email: existUser.dataValues.email,
-      };
-      const token = createToken(userToken);
-      res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
-      return res.status(200).json({ user: userToken.username });
-    } catch (error) {
-      res.status(500).send(error);
+  try {
+    const existUser = await getUserByEmail(email);
+    if (!existUser) {
+      return res.status(404).send("Incorrect email or password");
     }
+    const auth = compareSync(password, existUser.dataValues.password);
+    if (!auth) {
+      return res.status(404).send("Incorrect email or password");
+    }
+    const userToken = {
+      username: existUser.dataValues.userName,
+      email: existUser.dataValues.email,
+    };
+    const token = createToken(userToken);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    return res.status(200).json({ user: userToken.username });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const logout_get = (req: Request, res: Response) => {
+  res.cookie("jwt", "logged out", { maxAge: 1 });
+  res.status(200).send("user logged out");
 };
