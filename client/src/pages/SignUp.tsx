61a4fe9936adb3fc9fd0bb96385ad3a9093ea8c5
omer rokach <77382841+omerokach@users.noreply.github.com>
2021-07-07 10:14:55 +0300

    loader on signup

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -3,6 +3,15 @@ import Button from "react-bootstrap/Button";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useAuth } from "../context/AuthContext";
 import { useHistory } from "react-router-dom";
+import GridLoader from "react-spinners/GridLoader";
+import { css } from "@emotion/react";
+import { useState } from "react";
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 type FormValues = {
   username: string;
@@ -11,16 +20,20 @@ type FormValues = {
 };
 
 export const SignUp = () => {
+  const [loading, setLoading] = useState(false);
   const history = useHistory();
   const { register, handleSubmit } = useForm<FormValues>();
   const { signup, setCurrentUser } = useAuth();
 
   const submitForm: SubmitHandler<FormValues> = async (data) => {
     try {
+      setLoading(true);
       const res = await signup(data.email, data.username, data.password);
+      setLoading(false);
       setCurrentUser(res.data);
       history.push("/");
     } catch (error) {
+      setLoading(false);
       console.log(error.response.data);
     }
   };
@@ -31,43 +44,50 @@ export const SignUp = () => {
 
   return (
     <div className="login-signup-container">
-      <div className="login-signup-card" style={{background:"white"}}>
-        <h1>Sign Up</h1>
-        <Form onSubmit={handleSubmit(submitForm)}>
-          <Form.Group className="mb-3">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              required
-              {...register("email")}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Username</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter username"
-              required
-              {...register("username")}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Enter password"
-              required
-              {...register("password")}
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Sign Up
-          </Button>
-        </Form>
+      <div className="login-signup-card" style={{ background: "white" }}>
+        {!loading ? (
+          <>
+            <h1>Sign Up</h1>
+            <Form onSubmit={handleSubmit(submitForm)}>
+              <Form.Group className="mb-3">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  required
+                  {...register("email")}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter username"
+                  required
+                  {...register("username")}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter password"
+                  required
+                  {...register("password")}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Sign Up
+              </Button>
+            </Form>
+          </>
+        ) : (
+          <div className="login-spinner-container">
+            <GridLoader size={94} color={"rgb(76, 87, 95)"} css={override} />
+          </div>
+        )}
         <div className="login-signup-link">
-
-        <p onClick={anchorHandler}>Login Here</p>
+          <p onClick={anchorHandler}>Login Here</p>
         </div>
       </div>
     </div>
