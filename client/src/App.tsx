ef4c8fb8e7b28d5519f7bca05c5eb0e2af379f6a
omer rokach <77382841+omerokach@users.noreply.github.com>
2021-07-07 11:16:33 +0300

    404 page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,6 +13,7 @@ import { Login } from "./pages/Login";
 import { AuthProvider } from "./context/AuthContext";
 import { DataProvider } from "./context/AppDataContext";
 import PrivateRoute from "./components/PrivateRoute";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <div className="App">
@@ -23,10 +24,10 @@ function App() {
               <PrivateRoute exact path="/" component={Main} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={SignUp} />
-              {/* <Route path="/404" component={NotFound} />
-            <Route>
-            <Redirect to="/404"></Redirect>
-          </Route> */}
+              <Route path="/404" component={NotFound} />
+              <Route>
+                <Redirect to="/404"></Redirect>
+              </Route>
             </Switch>
           </Router>
         </DataProvider>
---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ExposureZeroIcon from '@material-ui/icons/ExposureZero';
+
+function NotFound() {
+  return (
+    <div className="not-found-container">
+      <h1 className="h1-not-found">
+        4
+        <span className="span-not-found">
+          0
+        </span>
+        4
+      </h1>
+      <h2 className="h2-not-found" >Error: 404 page not found</h2>
+      <p className="p-not-found">Sorry, the page you're looking for cannot be accessed</p>
+    </div>
+  );
+}
+
+export default NotFound;
